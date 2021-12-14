c81777ee651af58d1f0090292fed510948c708fe
Annabel Krijgsman <annabelkrijgsman@gmail.com>
2021-12-14 18:27:16 +0100

    code clean-up

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -54,57 +54,55 @@ function Chart({ data, isLoading, name, difficultyChecked, enjoyChecked, lineCha
 
   return (
     <div className="chart">
-      <p>In the chart you are seeing the ratings from: <span>{name ? `${name}` : "all students"}</span></p>
-        <ResponsiveContainer width="100%" aspect={4}>
-          {!lineChartChecked ? 
-            <BarChart
-              data={name ? individualRatings : averageRatings}
-              margin={{
-                top: 5,
-                right: 50,
-                left: 0,
-                bottom: 5,
-              }}
-              
-            >
-              <CartesianGrid 
-                strokeDasharray="3 3" 
-                horizontal={true}
-                vertical={false}
+      <ResponsiveContainer width="100%" aspect={4}>
+        {!lineChartChecked ? 
+          <BarChart
+            data={name ? individualRatings : averageRatings}
+            margin={{
+              top: 5,
+              right: 50,
+              left: 0,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid 
+              strokeDasharray="3 3" 
+              horizontal={true}
+              vertical={false}
+            />
+            <XAxis 
+              dataKey="assignment"
+              type="category"
+              tick={{ fontSize: 15 }}
+              tickFormatter={renderCustomTick}
+            />
+            <YAxis 
+              type="number"
+              allowDecimals={false}
+              tickCount={10}
+              domain={[0, 5]}
+              interval={0}
+              tickLine={false}
+            />
+            <Tooltip />
+            <Legend 
+              verticalAlign="top"
+            />
+            {difficultyChecked ? (
+              <Bar 
+                name="Difficulty"
+                dataKey="difficultyRating" 
+                fill='#D16A33'
               />
-              <XAxis 
-                dataKey="assignment"
-                type="category"
-                tick={{ fontSize: 15 }}
-                tickFormatter={renderCustomTick}
+            ) : null}
+            {enjoyChecked ? (
+              <Bar 
+                name="Enjoyment"
+                dataKey="enjoymentRating" 
+                fill='#A3C85A'
               />
-              <YAxis 
-                type="number"
-                allowDecimals={false}
-                tickCount={10}
-                domain={[0, 5]}
-                interval={0}
-                tickLine={false}
-              />
-              <Tooltip />
-              <Legend 
-                verticalAlign="top"
-              />
-              {difficultyChecked ? (
-                <Bar 
-                  name="Difficulty"
-                  dataKey="difficultyRating" 
-                  fill='#D16A33'
-                />
-              ) : null}
-              {enjoyChecked ? (
-                <Bar 
-                  name="Enjoyment"
-                  dataKey="enjoymentRating" 
-                  fill='#A3C85A'
-                />
-              ) : null}
-            </BarChart>
+            ) : null}
+          </BarChart>
         : 
           <LineChart
             width={500}
